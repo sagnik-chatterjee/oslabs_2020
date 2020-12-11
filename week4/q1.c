81cb7ed46030c016e1b16da48fe65d3b3f659a34
Sagnik Chatterjee <chatterjeesagnik88@gmail.com>
2020-12-12 00:18:06 +0530

    added files

--- week4/q1.c
+++ week4/q1.c
@@ -1,32 +1,34 @@
 /*
-
-AUTHOR :SAGNIK CHATTERJEE
-DATE : 11 DEC,2020
-USAGE : ./q1 q1file
-where q1file is the input file 
+AUTHOR: SAGNIK CHATTERJEE
+DATE : DEC 11,2020
+Usage : ./q1 
 
 */
 
-//to print the inode number of the file 
-
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc ,char **argv){
+int main(){
+	int status;
+	pid_t pid; 
+	pid=fork();
 
-	if(argc!=2){
-		printf("[ERROR] Usage : %s <file> \n",argv[0]);
-		exit(1);
+	//printf("The process id of the parent process is %d",getpid());
+	//printf("The process id of the child process is %d",pid);
+
+	if(pid==-1){
+		printf("[ERROR] Couldn't create child process !!\n");
 	}
-	char buffer[100];
-
-	bzero(buffer,sizeof(buffer));
-	strcat(buffer,"ls ");
-	strcat(buffer,"-i ");
-	strcat(buffer,argv[1]);
-	system(buffer);
-
-	return 0;
-
+	else if(pid==0){
+		printf("[STATUS] This is the child process,with process id :- %d\n,and the process id of its parent process is :- %d\n",getpid(),getppid());
+		exit(0);
+	}	
+	else {
+		wait(&status);
+		printf("[STATUS] This is the parent process ,with process id :- %d\n",getpid());
+		printf("[STATUS] Child process returns status :- %d\n",status);
+	}
+return 0;
 }
