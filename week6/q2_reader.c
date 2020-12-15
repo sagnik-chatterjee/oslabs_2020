e29161cb9a4da57f4910c56ce7e4e29cfa05e8d3
Sagnik Chatterjee <chatterjeesagnik88@gmail.com>
2020-12-15 06:49:30 +0530

    added files

---
+++ week6/q2_reader.c
@@ -0,0 +1,19 @@
+/*
+AUTHOR : SAGNIK CHATTERJEE
+DATE : DEC 15 , 2020
+USAGE :
+
+*/
+
+//the reader part -->has to be the child process
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+
+int main() {
+}
