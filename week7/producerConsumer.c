b77d1a46ba77c974013ae36814ebb36065ca45c7
Sagnik Chatterjee <chatterjeesagnik88@gmail.com>
2020-12-18 11:34:43 +0530

    added all files

--- week7/producerConsumer.c
+++ week7/producerConsumer.c
@@ -1,46 +1,51 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
 
-int buf[5],f,r;
-sem_t mutex,full,empty;
 
-void *produce(void *arg){
-	for(int i=0;i<10;i++){
-		sem_wait(&empty);
-		sem_wait(&mutex);
-		printf("Produced item is %d\n",i);
-		buf[(++r)%5]=i;
-		sleep(1);
-		sem_post(&mutex);
-		sem_post(&full);
-		printf("Full %u\n",full);
-	}
+int buf[10], f = 0,r = 0, item, val;
+sem_t mutex, full, empty;
+
+void *producer(void *arg){
+    int i;
+    for(i = 0;i < 10; i++){
+        sem_wait(&empty);
+        sem_wait(&mutex);
+        printf("Produced item is %d\n", i);
+        buf[(++r) % 10] = i;
+        sleep(1);
+        sem_post(&mutex);
+        sem_post(&full);
+        sem_getvalue(&full, &val);
+        printf("Full : %d \n", val);
+    }
 }
 
-void *consume(void *arg){
-	int item;
-	for(int i=0;i<10;i++){
-		sem_wait(&full);
-		printf("Full %u\n",full);
-		sem_wait(&mutex);
-		item=buf[(++f)%5];
-		printf("Consumed item is %d\n",item);
-		sleep(1);
-		sem_post(&mutex);
-		sem_post(&empty);
-	}
+void *consumer(void *arg){
+    int i;
+    for(i = 0; i < 10; i++){
+        sem_getvalue(&full, &val);
+        printf("Full:%d \n", val);
+        sem_wait(&full);
+        sem_wait(&mutex);
+        item = buf[(++f) % 10];
+        printf("Consuming the item %d\n", item);
+        sleep(1);
+        sem_post(&mutex);
+        sem_post(&empty);
+    }
 }
 
-int main(){
-	pthread_t tid1,tid2;
-	sem_init(&mutex,0,1);
-	sem_init(&full,0,1);
-	sem_init(&empty,0,5);
-
-	pthread_create(&tid1,NULL,produce,NULL);
-	pthread_create(&tid2,NULL,consume,NULL);
-	pthread_join(tid1,NULL);
-	pthread_join(tid2,NULL);
-	return 0;
+int main(int argc, char const *argv[]){
+    pthread_t tid1, tid2;
+    sem_init(&mutex, 0, 1);
+    sem_init(&full, 0, 1);
+    sem_init(&empty, 0, 10);
+    pthread_create(&tid1, NULL, producer, NULL);
+    pthread_create(&tid2, NULL, consumer, NULL);
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    return 0;
 }
